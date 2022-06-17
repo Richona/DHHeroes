5d61db38a9c72ac53191d3bff7b3b85077790548
Richona <105828822+Richona@users.noreply.github.com>
2022-06-16 22:22:22 -0300

    agregue un recurso estatico de express en app

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express();
 const path = require("path");
 const port = 3030;
 
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, "views", "index.html")))
-app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname, "views", "babbage.html")))
-app.get('/berners-lee', (req,res) => res.sendFile(path.join(__dirname, "views", "berners-lee.html")))
-app.get('/clarke', (req,res) => res.sendFile(path.join(__dirname, "views", "clarke.html")))
-app.get('/hamilton', (req,res) => res.sendFile(path.join(__dirname, "views", "hamilton.html")))
-app.get('/hopper', (req,res) => res.sendFile(path.join(__dirname, "views", "hopper.html")))
-app.get('/loverace', (req,res) => res.sendFile(path.join(__dirname, "views", "loverace.html")))
-app.get('/turing', (req,res) => res.sendFile(path.join(__dirname, "views", "turing.html")))
+app.use(express.static("public"));
 
-app.listen(port, () => console.log('Server running in http://localhost:' + port))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, "views", "index.html")));
+app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname, "views", "babbage.html")));
+app.get('/berners-lee', (req,res) => res.sendFile(path.join(__dirname, "views", "berners-lee.html")));
+app.get('/clarke', (req,res) => res.sendFile(path.join(__dirname, "views", "clarke.html")));
+app.get('/hamilton', (req,res) => res.sendFile(path.join(__dirname, "views", "hamilton.html")));
+app.get('/hopper', (req,res) => res.sendFile(path.join(__dirname, "views", "hopper.html")));
+app.get('/loverace', (req,res) => res.sendFile(path.join(__dirname, "views", "loverace.html")));
+app.get('/turing', (req,res) => res.sendFile(path.join(__dirname, "views", "turing.html")));
+
+app.listen(port, () => console.log('Server running in http://localhost:' + port));
